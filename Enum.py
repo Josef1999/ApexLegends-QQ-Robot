fbc0f5e8fed26bb73edf60107819cb9119a18e6c
源源球球✨ <1340793687@outlook.com>
2022-05-26 18:10:32 +0800

    Update Enum.py

--- Enum.py
+++ Enum.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 class CmdEnum(Enum):
     RECORD = "查询战绩"
-    BIND = "绑定账号"
+    BIND = "绑定id"
     MAP = "当前地图"
     CRAFTING = "当前制造"
 
 class UsageEnum(Enum):
-    RECORD = '.{cmd} EA_ID [若已绑定EA账号,可不输入EA账号]'.format(cmd=CmdEnum.RECORD.value)
-    BIND = '.{cmd} EA_ID [自动将QQ号与EA账号绑定]'.format(cmd=CmdEnum.BIND.value)
+    RECORD = '.{cmd} EA_ID [若已绑定EA id,可不输入EA id]'.format(cmd=CmdEnum.RECORD.value)
+    BIND = '.{cmd} EA_ID [自动将QQ号与EA id绑定]'.format(cmd=CmdEnum.BIND.value)
     MAP = '.{cmd}'.format(cmd=CmdEnum.MAP.value)
     CRAFTING = '.{cmd}'.format(cmd=CmdEnum.CRAFTING.value)
